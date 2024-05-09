88c78942d5466d19f5f0c1b8c3bc7e6699aa5942
Shutirtha Roy <shutirtha.roy1@gmail.com>
2024-05-10 00:45:01 +1000

    University list implemented

--- src/frontend/InstatuteApp/src/app/pages/admin/university/university.component.ts
+++ src/frontend/InstatuteApp/src/app/pages/admin/university/university.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UniversityService } from 'src/app/services/university.service';
 
 @Component({
   selector: 'app-university',
   templateUrl: './university.component.html',
   styleUrls: ['./university.component.css']
 })
-export class UniversityComponent {
+export class UniversityComponent  implements OnInit{
+  universityList: any[] = [
+    {
+      "title": "Swinburne University of Technology",
+      "description": "Best for IT related courses with high QS ranking.",
+      "tutor_list": [
+        "Samin", "Kabir"
+      ],
+      "course_list": [
+        "Physics", "Maths"
+      ]
+    }
+  ];
 
+  constructor(
+    private universityService: UniversityService) { 
+
+  }
+
+  ngOnInit(): void {
+    this.universityService.getAllUniversities()
+    .subscribe({
+      next: (universityResponse: any) => {
+        this.universityList = universityResponse.result;
+      },
+      error: (response) => {
+        console.log(response)
+      }
+    })
+  }
+
+  deleteUniversities(title: string) {
+    this.universityService.deleteUniversity(title)
+    .subscribe({
+      next: (response) => {
+        this.ngOnInit();
+        console.log(response)
+      },
+      error: (response) => {
+        console.log(response)
+      }
+    });
+  }
 }
---
+++ src/frontend/InstatuteApp/src/app/services/university.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UniversityService {
+  private baseUrl: string = "/api/v1/university/";
+
+  constructor(
+    private http: HttpClient, 
+    private router: Router) { 
+  }
+
+  getAllUniversities() {
+    return this.http.get(`${this.baseUrl}getalluniversity`);
+  }
+
+  deleteUniversity(title: string) {
+    return this.http.delete(`${this.baseUrl}${title}`);
+  }
+}
