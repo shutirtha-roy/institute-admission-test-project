e1179d777c19baa42c4fb95f35ba81500b86165b
Shutirtha Roy <shutirtha.roy1@gmail.com>
2024-05-16 13:58:17 +1000

    student performance implemented

--- src/frontend/InstatuteApp/src/app/pages/student/performance/performance.component.ts
+++ src/frontend/InstatuteApp/src/app/pages/student/performance/performance.component.ts
@@ -10,6 +10,7 @@ import { SessionService } from 'src/app/services/session.service';
 })
 export class PerformanceComponent implements OnInit {
   studentPerformanceList!: any[];
+  filteredResultsByCourse: { [courseCode: string]: any[] } = {};
 
   constructor(
     private authService: AuthService,
@@ -29,18 +30,28 @@ export class PerformanceComponent implements OnInit {
         next: (response: any) => {
           this.studentPerformanceList = response.result
           const courseCodesToFilter: string[] = Array.from(new Set(this.studentPerformanceList.map(result => result.course.course_code)));
-          const filteredResultsByCourse: { [courseCode: string]: any[] } = {};
+          this.filteredResultsByCourse = {};
 
           courseCodesToFilter.forEach(courseCode => {
               const filteredResultsForCourse: any[] = this.studentPerformanceList.filter(result => result.course.course_code === courseCode);
-              filteredResultsByCourse[courseCode] = filteredResultsForCourse;
+              this.filteredResultsByCourse[courseCode] = filteredResultsForCourse;
           });
 
-          console.log(filteredResultsByCourse);
+          console.log(this.filteredResultsByCourse);
         },
         error: (response) => {
           console.log(response)
         }
     });
+  }
+
+  getOverallPercentage(quizList: any[]) {
+    let sum = 0;
+
+    quizList.forEach((quiz) => {
+      sum += quiz.quiz_score_percent;
+    })
+
+    return `${sum/quizList.length}%`;
   }
 }
